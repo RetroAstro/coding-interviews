9f4e902d7b159f9231d978ce811d911a489b3cd0
RetroAstro <452461114@qq.com>
2020-01-17 18:26:27 +0800

    update

---
+++ 从尾到头打印链表.js
@@ -0,0 +1,20 @@
+/*function ListNode(x) {
+    this.val = x
+    this.next = null
+}*/
+
+function printListFromTailToHead(head) {
+  let arr = []
+  let curr = head
+
+  while (curr != null) {
+    if (arr.length) {
+      arr.unshift(curr.val)
+    } else {
+      arr.push(curr.val)
+    }
+    curr = curr.next
+  }
+
+  return arr
+}
