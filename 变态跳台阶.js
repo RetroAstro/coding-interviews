9133c1f4a3095e39ebee84abde7301f186c3dddd
RetroAstro <452461114@qq.com>
2020-01-20 17:32:14 +0800

    update

--- 变态跳台阶.js
+++ 变态跳台阶.js
@@ -1,4 +1,14 @@
-function jumpFloorII(number) {
+let map = {}
 
+function jumpFloorII(number) {
+  if (number == 0 || number == 1) return 1
+  if (number == 2) return 2
+  if (map[number]) return map[number]
+  let res = 0
+  for (let i = 1; i <= number; i++) {
+    res += jumpFloorII(number - i)
+  }
+  map[number] = res
+  return res
 }
 
