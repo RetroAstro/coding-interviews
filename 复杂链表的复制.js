a97e10b3856b9108fedad3c0125a1cdf6845b3a8
RetroAstro <452461114@qq.com>
2020-02-04 17:07:57 +0800

    update

---
+++ 复杂链表的复制.js
@@ -0,0 +1,30 @@
+/*function RandomListNode(x) {
+    this.label = x
+    this.next = null
+    this.random = null
+}*/
+function clone(head) {
+  let map = new WeakMap()
+
+  let curr1 = head
+  while (curr1 != null) {
+    map.set(curr1, new Node(curr1.label))
+    curr1 = curr1.next
+  }
+
+  let curr2 = head
+  while (curr2 != null) {
+    let node = map.get(curr2)
+    node.next = map.get(curr2.next)
+    node.random = map.get(curr2.random)
+    curr2 = curr2.next
+  }
+
+  return map.get(head)
+
+  function Node(x) {
+    this.label = x
+    this.next = null
+    this.random = null
+  }
+}
