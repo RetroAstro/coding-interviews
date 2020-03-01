c9ff12a629596c2bdc1399c382c7ee056f409579
RetroAstro <452461114@qq.com>
2020-03-01 15:58:43 +0800

    update

--- 序列化二叉树.js
+++ 序列化二叉树.js
@@ -4,9 +4,51 @@
     this.right = null
 } */
 function serialize(root) {
+  if (root == null) {
+    return '[]'
+  }
 
+  let queue = [root]
+  let str = ''
+
+  while (queue.length) {
+    let node = queue.shift()
+    if (node) {
+      str += `${node.val},`
+      queue.push(node.left)
+      queue.push(node.right)
+    } else {
+      str += `#,`
+    }
+  }
+
+  return `[${str}]`
 }
 
 function deserialize(s) {
+  if (s.length <= 2) {
+    return null
+  }
 
+  let arr = s.substr(1, s.length - 1).split(',')
+  let root = new TreeNode(arr.shift())
+  let queue = [root]
+
+  while (queue.length) {
+    let node = queue.shift()
+
+    let leftVal = arr.shift()
+    if (leftVal != '#') {
+      node.left = new TreeNode(leftVal)
+      queue.push(node.left)
+    }
+
+    let rightVal = arr.shift()
+    if (rightVal != '#') {
+      node.right = new TreeNode(rightVal)
+      queue.push(node.right)
+    }
+  }
+  
+  return root
 }
